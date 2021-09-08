6422dc6e22816f13f1a81f66129049fc1c2a2e43
Johann Hagler <jha@mission-embedded.com>
2021-09-08 17:22:37 +0200

    sed regex replace added

---
+++ bash/replace_regex.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+# g - global replacement
+
+namespace=wcep::alarm_and_error_state
+namespace=$namespace::MessageHeader
+
+sed -i 's/#include "json.hpp"/#include "common\/message_framework\/json\/messages.h"/' $1
+
+sed -i "s/$namespace MessageHeader;/messaging::json::MessageHeader MessageHeader;/" $1
+sed -i "s/x.MessageHeader = j.at(\"messageHeader\").get<$namespace>/x.MessageHeader = j.at(\"messageHeader\").get<messaging::json::MessageHeader>/" $1
+
+sed -i 's/namespace detail {//' $1
+# remove last "}" in the file
+sed -i '$ s/.$//' $1
