327c72d3215efcb8ba68b64fb118b411fb22f8b9
Johann Hagler <johann.hagler@gmail.com>
2021-10-07 14:53:19 +0200

    nlohmann::json update

--- C++/3rd Party Libraries/nlohmann json/main.cpp
+++ C++/3rd Party Libraries/nlohmann json/main.cpp
@@ -3,20 +3,113 @@
 
 #include <nlohmann/json.hpp>
 
-//-----------------------------------------------------------------------------
-int main()
+std::string json_msg = R"(
 {
-    // create a JSON array
-    nlohmann::json j1 = {"one", "two", 3, 4.5, false};
+  "messageHeader": {
+    "messageType": "gd.periodical.gasValues",
+    "messageVersion": "1.0",
+    "messageId": "c832655d-8f87-4c6f-b60b-e51250dbdfe3",
+    "createdAt": "2021-08-30T08:55:56.420Z"
+  },
+  "messageBody": {
+    "gasValues": [
+    {
+      "physicalDeviceId": "00:11:22:33:FF:01",
+      "measurementTimestamp": "2021-12-17T09:30:47.000Z",
+      "sensorChannels": [
+        {
+          "channelNumber": 0,
+          "gasType": "ch4",
+          "gasUnit": "%UEG",
+          "gasValue": "0"
+        },
+        {
+          "channelNumber": 2,
+          "gasType": "O2",
+          "gasUnit": "Vol%",
+          "gasValue": "20.9"
+        },
+        {
+          "channelNumber": 3,
+          "gasType": "CO",
+          "gasUnit": "ppm",
+          "gasValue": "0"
+        },
+        {
+          "channelNumber": 4,
+          "gasType": "H2S",
+          "gasUnit": "ppm",
+          "gasValue": "0"
+        }
+      ]
+    },
+    {
+      "physicalDeviceId": "00:11:22:33:FF:02",
+      "measurementTimestamp": "2021-12-17T09:31:47.000Z",
+      "sensorChannels": [
+        {
+          "channelNumber": 0,
+          "gasType": "ch4",
+          "gasUnit": "%UEG",
+          "gasValue": "0"
+        },
+        {
+          "channelNumber": 2,
+          "gasType": "O2",
+          "gasUnit": "Vol%",
+          "gasValue": "20.9"
+        },
+        {
+          "channelNumber": 3,
+          "gasType": "CO",
+          "gasUnit": "ppm",
+          "gasValue": "0"
+        },
+        {
+          "gasType": "H2S",
+          "gasUnit": "ppm",
+          "gasValue": "0"
+        }
+      ]
+    }
+    ]
+  }
+}
+  )";
 
-    // create a copy
-    nlohmann::json j2(j1);
+void ConvertMessage(const std::string& msg) {
+  nlohmann::json j_in = nlohmann::json::parse(msg);
+  nlohmann::json j_out;
 
-    // serialize the JSON array
-    std::cout << j1 << " = " << j2 << '\n';
-    std::cout << std::boolalpha << (j1 == j2) << '\n';
+  j_out["messageHeader"] = "Header";
 
-    std::string json_str = R"(
+  auto gasValues = j_in["messageBody"]["gasValues"];
+  for (auto gasValue : gasValues) {
+    nlohmann::json j;
+    j["gasDetectorId"] = "e0a074f1-f321-4910-a5a5-5a721ba7f44a";
+    j["measurementTimestamp"] = "2021-12-17T09:30:47.000";
+
+    for (auto sensorChannel : gasValue["sensorChannels"])
+      j["sensorChannels"].push_back(sensorChannel);
+
+    j_out["messageBody"]["gasValues"].push_back(j);
+  }
+
+  std::cout << j_out.dump();
+}
+
+void BasicStuff() {
+  // create a JSON array
+  nlohmann::json j1 = {"one", "two", 3, 4.5, false};
+
+  // create a copy
+  nlohmann::json j2(j1);
+
+  // serialize the JSON array
+  std::cout << j1 << " = " << j2 << '\n';
+  std::cout << std::boolalpha << (j1 == j2) << '\n';
+
+  std::string json_str = R"(
 {
   "messageHeader": {
     "messageType": "gasValues",
@@ -26,10 +119,33 @@ int main()
   }}
     )";
 
-    nlohmann::json j3 = nlohmann::json::parse(json_str);
+  nlohmann::json j3 = nlohmann::json::parse(json_str);
 
-    auto messageType = j3["messageHeader"]["messageType"];
+  auto messageType = j3["messageHeader"]["messageType"];
 
-    // auto hdr = j3.at("messageHeader").get();
-    std::cout << messageType << "\n";
+  // auto hdr = j3.at("messageHeader").get();
+  std::cout << messageType << "\n";
+
+  // write it almost like json text file:
+  nlohmann::json j_object = {
+    {"pi", 3.141},
+    {"happy", true},
+    {"name", "Niels"},
+    {"nothing", nullptr},
+    {"answer", {
+      {"everything", 42}
+    }},
+    {"list", {1, 0, 2}},
+    {"object", {
+      {"currency", "USD"},
+      {"value", 42.99}
+    }}
+  };
+}
+
+//-----------------------------------------------------------------------------
+int main()
+{
+  // BasicStuff();
+  ConvertMessage(json_msg);
 }
