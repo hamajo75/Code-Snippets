addea092835385c652d1987c0207d174e79bacd6
Johann Hagler <johann.hagler@gmail.com>
2021-11-03 15:36:11 +0100

    C++ funtion overloading ==

--- C++/Functions/Operator Overloading/main.cpp
+++ C++/Functions/Operator Overloading/main.cpp
@@ -12,6 +12,7 @@ class Point {
       : m_x{x}, m_y{y}, m_z{z} {}
 
   friend std::ostream &operator<<(std::ostream &out, const Point &point);
+  friend bool operator==(const Point& lhs, const Point& rhs);
 };
 
 std::ostream &operator<<(std::ostream &out, const Point &point) {
@@ -20,15 +21,32 @@ std::ostream &operator<<(std::ostream &out, const Point &point) {
 
   return out;  // return std::ostream so we can chain calls to operator<<
 }
+
+bool operator==(const Point& lhs, const Point& rhs) {
+  return (lhs.m_x == rhs.m_x &&
+          lhs.m_x == rhs.m_x &&
+          lhs.m_x == rhs.m_x);
+}
+
 }
 
 using my_namespace::Point;
 using my_namespace::operator<<;
 
-int main() {
+void StreamPoint() {
   const Point point1{2.0, 3.0, 4.0};
-
   std::cout << point1 << '\n';
+}
+void ComparePoint() {
+  Point point1{2.0, 3.0, 4.0};
+  Point point2{2.0, 3.0, 4.0};
+
+  if (point1 == point2)
+    std::cout << "equal points\n";
+}
+int main() {
+  StreamPoint();
+  ComparePoint();
 
   return 0;
 }
