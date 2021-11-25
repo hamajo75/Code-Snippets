0aeeb9f35e9afdcef48b7db3240f7f5add58e2a0
Joe <johann.hagler@gmail.com>
2021-11-25 20:57:54 +0100

    C++ enum class improved

--- C++/Sandbox/EnumClass/main.cpp
+++ C++/Sandbox/EnumClass/main.cpp
@@ -12,12 +12,12 @@ class EnumerationClass {
   EnumMap mapping_;
 
   std::string GetIndexedRepresentation(T enum_value, int index) {
-    for (auto entry : mapping_)
+    for (auto entry : mapping_) {
       if (std::get<0>(entry) == enum_value) {
         auto representations = std::get<1>(entry);
         return representations[index];
       }
-
+    }
     return "";
   }
 
@@ -28,6 +28,17 @@ class EnumerationClass {
   std::string GetRepresentation(T enum_value, int index) {
     return GetIndexedRepresentation(enum_value, index);
   }
+
+  T GetEnumValue(const std::string& str_representation) {
+    for (auto entry : mapping_) {
+      for (auto value : std::get<1>(entry)) {
+        if (value == str_representation)
+          return std::get<0>(entry);
+      }
+    }
+    return std::get<0>(mapping_[0]);
+  }
+
 };
 
 enum class VideoResolution {kHigh, kLow, kUnknown};
@@ -42,6 +53,9 @@ int main() {
   std::cout << "representation 0: " << my_enum.GetRepresentation(VideoResolution::kLow, 0) << "\n";
   std::cout << "representation 1: " << my_enum.GetRepresentation(VideoResolution::kLow, 1) << "\n";
   std::cout << "representation 2: " << my_enum.GetRepresentation(VideoResolution::kLow, 2) << "\n";
+
+  if (my_enum.GetEnumValue("unknown") == VideoResolution::kUnknown)
+    std::cout << "unknown\n";
 
   return 0;
 }
