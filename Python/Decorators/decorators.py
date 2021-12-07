a472d60a6d67c5de8c2d0d59a13000b7bdc83fb2
Johann Hagler <johann.hagler@gmail.com>
2021-12-07 17:22:28 +0100

    python decorators

---
+++ Python/Decorators/decorators.py
@@ -0,0 +1,15 @@
+# A decorator wraps around another function and adds some
+# functionality to it.
+
+# decorator function
+def do_twice(func):
+    def wrapper_do_twice(*args, **kwargs):
+        func(*args, **kwargs)
+        func(*args, **kwargs)
+    return wrapper_do_twice
+
+@do_twice
+def greet(name):
+    print(f"Hello {name}")
+
+greet("World")
