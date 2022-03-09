e40ec75d0620db12e6adc624d4f72a2422ed5c4c
jha <jha@mission-embedded.com>
2022-03-09 11:40:26 +0100

    python: template tool

--- bash/files.sh
+++ bash/files.sh
@@ -2,16 +2,21 @@
 
 # loop through all files in folder
 for file in *.txt ; do
-  mv "$file" "$file.bak"
+  echo processing "$file"
 done
 
+# read lines from file
+station_idx=$(head -n 1 "file.txt")
+echo $station_idx
+
+# read whole file into variable
+value=`cat file.txt`
+echo "$value"
+
+exit 0
 # Monitor changes in a folder:
 while inotifywait -e modify /var/log/messages; do
   if tail -n1 /var/log/messages | grep apache; then
     kdialog --msgbox "Blah blah Apache"
   fi
 done
-
-# read from file
-station_idx=$(head -n 1 "file.txt")
-echo $station_idx
