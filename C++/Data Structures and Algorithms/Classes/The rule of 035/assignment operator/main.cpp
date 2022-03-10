77375b547424c58d0a383b8b6a3708948abe036b
jha <jha@mission-embedded.com>
2022-03-10 13:00:07 +0100

    c++: assignment operator

--- C++/Data Structures and Algorithms/Classes/The rule of 035/assignment operator/main.cpp
+++ C++/Data Structures and Algorithms/Classes/The rule of 035/assignment operator/main.cpp
@@ -1,11 +1,14 @@
 #include <iostream>
 #include <string>
+#include <optional>
+#include <vector>
 
 struct SensorChannelA {
   int channel_number;
   std::string gas_type;
   std::string gas_unit;
   std::string gas_value;
+  void operator=(SensorChannelA& other);
 };
 
 struct SensorChannelB {
@@ -22,22 +25,74 @@ struct SensorChannelB {
   }
 };
 
-SensorChannelB& SensorChannelA::operator=() {
-  SensorChannelB b;
-  b.channel_number = this->channel_number;
-  b.gas_type = this->gas_type;
-  b.gas_unit = this->gas_unit;
-  b.channel_number = this->channel_number;
-
-  return b;
+void SensorChannelA::operator=(SensorChannelA& other) {
+  other.channel_number = this->channel_number;
+  other.gas_type = this->gas_type;
+  other.gas_unit = this->gas_unit;
+  other.gas_value = this->gas_value;
 }
-//-------------------------------------------------------------------------------
-int main() {
+
+void SensorChannelExample() {
   SensorChannelA a = {0, "typeA", "", ""};
   SensorChannelB b;
   b = a;
 
   std::cout << "b.gas_type " << b.gas_type << "\n";
+}
+
+struct Alarm {
+  std::optional<std::string> additional_information;
+  std::string alarm_type;
+  std::optional<std::string> device_id;
+};
+
+using Alarms = std::vector<Alarm>;
+
+struct MyAlarm : public Alarm {
+  std::string owner;
+
+  MyAlarm() = default;
+  MyAlarm& operator=(const Alarm& other) {
+    this->alarm_type = other.alarm_type;
+    this->device_id = other.device_id;
+    this->additional_information = other.additional_information;
+
+    return *this;
+  }
+  explicit MyAlarm(const Alarm& other) {
+    *this = other;
+  }
+};
+
+struct MyAlarms : public std::vector<MyAlarm> {
+  void operator=(const Alarms& other) {
+    for (const auto& other_alarm : other) {
+      this->push_back(MyAlarm{other_alarm});
+    }
+  }
+  explicit MyAlarms(const Alarms& other) {
+    *this = other;
+  }
+};
+
+void alarm_fun(MyAlarms alarms) {
+  for (const auto& alarm : alarms) {
+    std::cout << "alarm.device_id" << alarm.alarm_type << "\n";
+  }
+}
+
+//-------------------------------------------------------------------------------
+int main() {
+  // SensorChannelExample();
+  Alarm a;
+  MyAlarm ma1 = static_cast<MyAlarm>(a);   // static_cast is necessary
+  MyAlarm ma{a};
+  ma = a;
+  ma.operator=(a);
+
+  Alarms alarms;
+  // alarm_fun(alarms);                         // static_cast is necessary
+  alarm_fun(static_cast<MyAlarms>(alarms));
 
   return 0;
 }
