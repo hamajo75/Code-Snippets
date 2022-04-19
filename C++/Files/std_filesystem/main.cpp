627ef67802f7fd6e80f1759c6a427be81bedf97e
Johann Hagler <jha@mission-embedded.com>
2022-04-19 18:10:50 +0200

    c++: google test capture stdout

--- C++/Files/std_filesystem/main.cpp
+++ C++/Files/std_filesystem/main.cpp
@@ -5,8 +5,18 @@
 #include <filesystem>
 
 void Files() {
+  std::filesystem::path file_path{"file.txt"};
+  file_path.is_relative();
+  std::cout << file_path.root_path() << "\n";
+  std::cout << file_path.extension() << "\n";
+  std::cout << file_path.filename() << "\n";
+  std::cout << "file exists: " << std::boolalpha << std::filesystem::exists(file_path) << "\n";
 
-  std::cout << "file exists: " << std::boolalpha << std::filesystem::exists("file.txt") << "\n";
+
+  auto path = std::filesystem::path{file};
+  auto new_path = path;
+  new_path.replace_filename("bak_" + path.filename().string());
+  std::filesystem::copy_file(path, new_path);
 }
 
 void Directories() {
@@ -16,7 +26,7 @@ void Directories() {
 
 //-------------------------------------------------------------------------------
 int main() {
-  Directories();
+  // Directories();
   Files();
 
   return 0;
