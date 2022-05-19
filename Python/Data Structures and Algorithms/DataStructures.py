55cdb6d0e6439ee41d7484d61a45f18c8cee45ed
Johann Hagler <jha@mission-embedded.com>
2022-05-19 08:34:42 +0200

    python: list comprehension

--- Python/Data Structures and Algorithms/DataStructures.py
+++ Python/Data Structures and Algorithms/DataStructures.py
@@ -25,14 +25,6 @@ print('Length: ' + str(len(s)))
 x = ['pig', 'cow', 'horse']
 a, b, c = x
 
-# list - sortable
-print('- list')
-# - list comprehension (cleverly create lists)
-#   put a loop and some conditions inside []
-x = [m for m in range(8)]
-x = [z**2 for z in range(10) if z%2]  # 1, 3, 5, 7, 9 will be squared
-print(x)
-
 x.insert(1, ['a', 'm'])  # position 1
 print(x.pop(1))
 
---
+++ Python/README.md
@@ -0,0 +1,5 @@
+# Python
+
+## Hot topics
+- [Decorators](Decorators/decorators.py)
+- [List Comprehension](Data%20Structures%20and%20Algorithms/list_comprehension.py)
