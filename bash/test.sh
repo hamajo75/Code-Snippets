2df3d012654729b5a0f722cccb9593330dd8fbf9
hamajo75 <johann.hagler@gmail.com>
2022-06-14 13:31:36 +0200

    bash: update conditions

--- bash/test.sh
+++ bash/test.sh
@@ -7,3 +7,21 @@
 test -z "aaa"  # is equivalent to
 [ -z "aaa" ]
 echo $?
+
+# a shell if works like this
+if
+   command-list1
+then
+   command-list2
+else
+   command-list3
+fi
+
+# then-clause is executed when the command-list1 exit code is 0
+# else-clause is executed when the command-list1 exit code is is non-0
+
+# if [ condition ]     - test
+# if [[ condition ]]   - test with extended features (not POSIX compatible)
+# if ( command )       - command runs in a subshell and sets it's exit code
+# if (( command ))     - arithmetic evaluation:  If the value of the expression is non-zero, the return status is 0; 
+#                        otherwise the return status is 1. (not POSIX compatible)
