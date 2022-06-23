d0a49233bbd364e0b94e97ce784210c649c66b90
Johann Hagler <jha@mission-embedded.com>
2022-06-23 17:42:53 +0200

    bash: loops

--- bash/loops.sh
+++ bash/loops.sh
@@ -1,21 +1,26 @@
-#!/bin/sh
+#!/bin/bash
 
 while [[ -e wait.txt ]] ; do
   sleep 3 # "sleep" for three seconds
 done
 
-for i in {1..20} ; do
+for i in {1..3} ; do
   echo "$i"
 done
 
-while true; do
-   # do something
-
-   # -n 1 to get one character at a time, -t 0.1 to set a timeout
-   read -n 1 -t 0.1 input                  # so read doesn't hang
-   if [[ $input = "q" ]]
-   then
-      echo # to get a newline after quitting
-      break
-   fi
+iterations=3
+for i in $(seq 1 ${iterations}); do  # this also works in sh
+  echo "$i"
 done
+
+# while true; do
+#    # do something
+
+#    # -n 1 to get one character at a time, -t 0.1 to set a timeout
+#    read -n 1 -t 0.1 input                  # so read doesn't hang
+#    if [[ $input = "q" ]]
+#    then
+#       echo # to get a newline after quitting
+#       break
+#    fi
+# done
