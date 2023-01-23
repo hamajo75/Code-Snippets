bbdbe5cd8862dc6e5b80c798eb6375d3e0ff8004
Johann Hagler <jha@mission-embedded.com>
2023-01-23 13:08:37 +0000

    bash: arrays

--- bash/Data/arrays.sh
+++ bash/Data/arrays.sh
@@ -7,3 +7,11 @@ echo ${#MY_ARRAY[@]}  # get nr. of entries
 for entry in ${MY_ARRAY[@]}; do echo "${entry}"; done
 for E in ${MY_ARRAY[@]}; do LINE+="${E}"; done
 echo ${LINE}
+
+# not in sh
+MY_ARRAY=(entry1 entry2 entry3)
+echo Print entries in MY_ARRAY: ${MY_ARRAY[@]}
+if [[ ! "${MY_ARRAY[*]}" =~ "entry4" ]]; then
+  echo "Entry not found." >&2
+  exit 1
+fi
