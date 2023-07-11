9f7bc9eba631c5ae215a0904960736a689f89bf0
Johann Hagler <jha@mission-embedded.com>
2023-07-11 15:48:58 +0000

    c++: add future examples

--- C++/Concurrency/std_future and std_async/main.cpp
+++ C++/Concurrency/std_future and std_async/main.cpp
@@ -4,15 +4,25 @@
 
 //-----------------------------------------------------------------------------
 std::string helloFunction(const std::string& s) {
+  std::this_thread::sleep_for(std::chrono::seconds(2));
   return "Hello C++11 from " + s + ".";
 }
 //-----------------------------------------------------------------------------
 class HelloFunctionObject {
  public:
   std::string operator()(const std::string& s) const {
+    std::this_thread::sleep_for(std::chrono::seconds(2));
     return "Hello C++11 from " + s + ".";
   }
 };
+
+std::future<int> async_function() {
+  return std::async(std::launch::async, []() {
+    std::this_thread::sleep_for(std::chrono::seconds(1));
+    return 42;
+  });
+}
+
 //-----------------------------------------------------------------------------
 int main() {
   std::cout << std::endl;
@@ -20,15 +30,23 @@ int main() {
   // future with function
   auto futureFunction = std::async(helloFunction, "function");
 
+  std::cout << "started function.\n";
+
   // future with function object
   HelloFunctionObject helloFunctionObject;
   auto futureFunctionObject =
       std::async(helloFunctionObject, "function object");
 
+  std::cout << "started object.\n";
+
   // future with lambda function
   auto futureLambda = std::async(
-      [](const std::string& s) { return "Hello C++11 from " + s + "."; },
+      [](const std::string& s) {
+        std::this_thread::sleep_for(std::chrono::seconds(2));
+        return "Hello C++11 from " + s + "."; },
       "lambda function");
+
+  std::cout << "started lambda.\n";
 
   // here we wait until all functions are finished
   std::cout << futureFunction.get() << "\n"
@@ -36,4 +54,10 @@ int main() {
             << futureLambda.get() << std::endl;
 
   std::cout << std::endl;
+
+
+  std::future<int> result_future = async_function();
+
+  int result = result_future.get();
+  std::cout << "Result: " << result << std::endl;
 }
