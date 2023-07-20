e67ee9f70109da1539371b5ad3ad81c2290c4c1e
Johann Hagler <jha@mission-embedded.com>
2023-07-20 11:00:07 +0000

    bash: maps

--- bash/Data/arrays.sh
+++ bash/Data/arrays.sh
@@ -12,8 +12,6 @@ for entry in ${MY_ARRAY[@]}; do echo "${entry}"; done
 for E in ${MY_ARRAY[@]}; do LINE+="${E}"; done
 echo ${LINE}
 
-
-
 # not in sh
 MY_ARRAY=(entry1 entry2 entry3)
 echo Print entries in MY_ARRAY: ${MY_ARRAY[@]}
@@ -21,6 +19,7 @@ if [[ ! "${MY_ARRAY[*]}" =~ "entry4" ]]; then
   echo "Entry not found." >&2
 fi
 
+# bash > version 4
 declare -A my_map
 my_map["interface"]="interface_value"
 my_map["port"]="port_value"
