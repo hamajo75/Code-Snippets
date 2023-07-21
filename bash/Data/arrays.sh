c0477cbfcbd43551cb549240fee10e4099ef266d
Johann Hagler <jha@mission-embedded.com>
2023-07-21 12:20:41 +0000

    bash: array example

--- bash/Data/arrays.sh
+++ bash/Data/arrays.sh
@@ -8,20 +8,22 @@ echo nr of entries: ${#MY_ARRAY[@]}
 MY_ARRAY[0]="c"
 MY_ARRAY[1]="d"
 
-for entry in ${MY_ARRAY[@]}; do echo "${entry}"; done
-for E in ${MY_ARRAY[@]}; do LINE+="${E}"; done
-echo ${LINE}
+for entry in "${MY_ARRAY[@]}"; do echo "${entry}"; done
+for E in "${MY_ARRAY[@]}"; do LINE+="${E}"; done
+echo "${LINE}"
+
+print_elements() {
+  local array=("$@")
+  echo Print entries in array: "${array[@]}"
+  if [[ ! "${array[*]}" =~ "entry4" ]]; then
+    echo "Entry not found." >&2
+  fi
+}
 
 # not in sh
 MY_ARRAY=(entry1 entry2 entry3)
-echo Print entries in MY_ARRAY: ${MY_ARRAY[@]}
-if [[ ! "${MY_ARRAY[*]}" =~ "entry4" ]]; then
-  echo "Entry not found." >&2
-fi
+print_elements "${MY_ARRAY[@]}"
 
-# bash > version 4
-declare -A my_map
-my_map["interface"]="interface_value"
-my_map["port"]="port_value"
-
-echo "${my_map["port"]}"
+# convert string to array
+arr=("one" "two" "three")
+echo "${arr[0]}"
--- bash/Data/strings.sh
+++ bash/Data/strings.sh
@@ -13,3 +13,12 @@ lowercase="abcdefg"
 echo ${lowercase^^}
 protocol="TCP"
 echo ${protocol~~}
+
+declare -A link2
+link2["destination"]="1.2.3.4"
+link2["port"]="22"
+cmd="iperf3 \
+  -c ${link2["destination"]} \
+  -p ${link2["port"]}"
+
+echo $cmd
