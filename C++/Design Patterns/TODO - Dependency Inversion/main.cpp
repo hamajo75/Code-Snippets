97ad679c1344fdd4d1a28ee9a8391bbf80e5e09f
Johann Hagler <johann.hagler@gmail.com>
2024-01-05 20:18:56 +0000

    c++: add empty folders for additional design patterns

---
+++ C++/Design Patterns/TODO - Dependency Inversion/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <string>
+
+// Controller(CommonBehaviour cb) vs
+// Controller: public CommonBehaviour
+
+//-------------------------------------------------------------------------------
+int main() {
+  return 0;
+}
