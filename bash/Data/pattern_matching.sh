3db035cd64b8ee39fe6250f5503a5e3e08804a7a
jha-mission <140061429+jha-mission@users.noreply.github.com>
2024-03-27 10:00:40 +0000

    bash: update example

--- bash/Data/pattern_matching.sh
+++ bash/Data/pattern_matching.sh
@@ -6,11 +6,12 @@
 # ## last or longest match
 # #  first or shortest match
 
-test="20.16.86.189:13002"
+test="bla:20.16.86.189:13002:hello"
 
-echo ${test##*:}  # jadlfj
-echo ${test#*:}   # dlfjaöbajadlfj
+# from left to right
+echo ${test##*:}  # 13002
+echo ${test#*:}   # 20.16.86.189:13002
 
 # from right to left: %
-echo ${test%%:*}  # ajdflj
-echo ${test%:*}   # ajdfljbadlfjaö
+echo ${test%%:*}  # bla
+echo ${test%:*}   # bla:20.16.86.189
