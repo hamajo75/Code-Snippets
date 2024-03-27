50ef4f138c1577861060dd57c087feeb3c978641
jha-mission <140061429+jha-mission@users.noreply.github.com>
2024-03-27 09:36:01 +0000

    bash: store cmd & args in separate variables

--- bash/starting_programs/store_cmd_in_string_or_array.sh
+++ bash/starting_programs/store_cmd_in_string_or_array.sh
@@ -1,16 +1,24 @@
 #!/bin/bash
 
-# store a cmd call in variable
-cmd="echo -e"
-cmd="$cmd Hello World\nnext line"
-#eval "$cmd"
-# no eval necessary:
-$cmd | grep World
+store_a_cmd_call_in_variable() {
+  cmd="echo -e"
+  cmd="$cmd Hello World\nnext line"
+  #eval "$cmd"
+  # no eval necessary:
+  $cmd | grep World
 
-cmd="echo -e"
-cmd="$cmd 'Hello World\nnext line'"         # here you need '' for the linebreak
-output=$(eval "$cmd" | grep World)          # store output in a variable
-echo "$output"
+  cmd="echo -e"
+  cmd="$cmd 'Hello World\nnext line'"         # here you need '' for the linebreak
+  output=$(eval "$cmd" | grep World)          # store output in a variable
+  echo "$output"
+}
+
+store_parts_of_a_command_in_variables() {
+  cmd="ls"
+  args="-a"
+  # execute like this
+  $cmd $args
+}
 
 # Notes:
 # Word splitting happens after variable expansion and command substitution.
@@ -22,11 +30,18 @@ echo "$output"
 # 5. Word splitting (spaces, tabs, and newlines)
 # 6. Pathname expansion
 
-# here you need eval (is eval evil?)
-cmd="echo -e 'Hello World\nnext line' | grep World"
-eval "$cmd"
+using_eval() {
+  # here you need eval (is eval evil?)
+  cmd="echo -e 'Hello World\nnext line' | grep World"
+  eval "$cmd"
+}
 
+store_command_and_params_in_array() {
+  cmd=(ls -a)
+  "${cmd[@]}"
+}
 
-# store cmd + params in an array
-cmd=(ls -la)
-"${cmd[@]}"
+store_a_cmd_call_in_variable
+store_parts_of_a_command_in_variables
+using_eval
+store_command_and_params_in_array
